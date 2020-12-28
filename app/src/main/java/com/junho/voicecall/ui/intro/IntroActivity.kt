ff927aa42bbd76d5e0b84b0ea0140bdc3d96ab33
PJH <kdh@KDHui-MacBookPro.local>
2020-12-28 20:06:17 +0900

    first commit

---
+++ app/src/main/java/com/junho/voicecall/ui/intro/IntroActivity.kt
@@ -0,0 +1,13 @@
+package com.junho.voicecall.ui.intro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.junho.voicecall.R
+import com.junho.voicecall.ui.base.BaseActivity
+
+class IntroActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
